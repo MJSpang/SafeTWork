9a133adca3a43a805a56212be5e3c67fa89ba742
MJSpang <ms11692@cabrini.edu>
2023-05-04 23:07:46 -0400

    updates

--- index.js
+++
@@ -1,6 +0,0 @@
-const searchToggle = document.getElementById("search-toggle");
-const searchForm = document.getElementById("search-form");
-
-searchToggle.addEventListener("click", () => {
-  searchForm.classList.toggle("show");
-});
